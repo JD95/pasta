83c77b6ea8797de9f112c30aa1d416e3df2eca74
Jeffrey Dwyer <jeffreydwyer95@outlook.com>
2019-09-16 21:32:20 -0700

    Adds unit tests for lambda and free variable

--- haskell-version/test/Spec.hs
+++ haskell-version/test/Spec.hs
@@ -22,14 +22,27 @@ tests = testGroup "Tests" . pure <$> unitTests
 unitTests :: IO TestTree
 unitTests = testSpec "Unit Tests" $ do
   describe "Type Checking" $ do
+
+      it "Accepts Free Variables in Context" $ do
+        let tbl              = Map.fromList [("x", mkCon ce "Thing")] 
+        let (e :: Fix CoreE) = mkFree ce "x" 
+        let (t :: Fix CoreE) = (mkCon ce "Thing")
+        either (const False) (const True) $ run (check runNoLogging tbl e t) 
+
+      it "Accepts Lambdas" $ do
+        let tbl              = mempty
+        let (e :: Fix CoreE) = mkLam ce () (mkVar ce 0)
+        let (t :: Fix CoreE) = mkArrow ce (Inline R1, Inline L) (mkCon ce "Thing") (mkCon ce "Thing")
+        either (const False) (const True) $ run (check runNoLogging tbl e t) 
+
       it "Accepts Valid Application" $ do
-        let tbl              = Map.fromList $ [("x", mkCon ce "Thing")]
+        let tbl              = Map.fromList [("x", mkCon ce "Thing")]
         let (e :: Fix CoreE) = mkApp ce (mkLam ce () (mkVar ce 0)) (mkFree ce "x")
         let (t :: Fix CoreE) = mkCon ce "Thing"
         either (const False) (const True) $ run (check runNoLogging tbl e t)
 
       it "Rejects Invalid Application" $ do
-        let tbl              = Map.fromList $ [("x", mkCon ce "Foo")]
+        let tbl              = Map.fromList [("x", mkCon ce "Foo")]
         let (e :: Fix CoreE) = mkApp ce (mkLam ce () (mkVar ce 0)) (mkFree ce "x")
         let (t :: Fix CoreE) = mkCon ce "Thing"
         either (const True) (const False) $ run (check runNoLogging tbl e t) 
