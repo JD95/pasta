a6b6f35cc9f39fe905b63c78f790f4e36c4e69ed
Jeff Dwyer <jdwyer@voceracommunications.com>
2019-10-17 16:00:04 -0700

    Represents first parse pass with Forest

--- haskell-version/src/Parse.hs
+++ haskell-version/src/Parse.hs
@@ -1,43 +1,40 @@
 module Parse where
 
-import Control.Monad
+import           Control.Monad
+import           Data.Tree
+import           Numeric.Natural
 
 printLineWithNumbers file = do
-  forM_ (zip [1..] (lines file)) $ \(n, line) ->
-    putStrLn $ (show n) <> ": " <> line
+  forM_ (zip [1 ..] (lines file))
+    $ \(n, line) -> putStrLn $ (show n) <> ": " <> line
 
-example = "hello\n"
-       <> "  world\n"
-       <> "I'm Jeff"
+example = "hello\n  world\nI'm Jeff"
 
-answer = [["hello", "  world"], ["I'm Jeff"]]
+answer = [Node "hello" [Node "  world" []], Node "I'm Jeff" []]
 
-isIndented :: String -> Bool
-isIndented line =
-  case line of
-    (' ':_) -> True 
-    _ -> False 
+getIndentation :: String -> Int
+getIndentation = length . takeWhile (' ' ==)
 
-block :: [String] -> [[String]]
+block :: [String] -> Forest String
 block [] = []
-block (next:rest) =
-  if not (isIndented next)
-    then let (subGroup, more) = span isIndented rest
-             newGroup = next : subGroup
-             otherGroups = block more 
-         in newGroup : otherGroups 
-    else error "Bad indentation"
+block (next : rest) =
+  let n                = getIndentation next
+      (subGroup, more) = span ((n <) . getIndentation) rest
+      newGroup         = Node next (block $ drop n <$> subGroup)
+      otherGroups      = block more
+  in  newGroup : otherGroups
 
 removeEmptyLines :: [String] -> [String]
-removeEmptyLines lines = filter (not . isEmpty) lines where
-  isEmpty [] = True
-  isEmpty (' ':rest) = isEmpty rest
-  isEmpty _ = False
+removeEmptyLines = filter (not . isEmpty)
+ where
+  isEmpty []           = True
+  isEmpty (' ' : rest) = isEmpty rest
+  isEmpty _            = False
 
 test :: IO ()
 test = do
   putStrLn "Starting Parsing..."
   file <- readFile "examples/Example1.jy"
-  let ls = removeEmptyLines $ lines file
+  let ls     = removeEmptyLines $ lines file
   let groups = block ls
-  forM_ groups print
+  forM_ groups (putStrLn . drawTree)
