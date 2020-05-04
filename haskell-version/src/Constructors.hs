25afbcc65b196d39cf12506252351f5f04489083
Jeffrey Dwyer <jeffreydwyer95@gmail.com>
2020-05-03 22:25:02 -0700

    Implementing basic evaluation

--- haskell-version/src/Constructors.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE RankNTypes #-}
-
-module Constructors where
-
-import           Data.Functor.Foldable
-
-mkMu :: (forall x . g x -> f x) -> (forall a . (f a -> a) -> g a) -> Mu f
-mkMu h g = Mu (\f -> f . h $ g f)
--- haskell-version/src/Lib.hs
+++ haskell-version/src/Lib.hs
@@ -1,53 +1,109 @@
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE LambdaCase #-}
 
-module Lib
-  ( someFunc
-  )
-where
+module Lib where
 
-import qualified Data.Map.Strict               as Map
-import           Data.Functor.Foldable
-import           Polysemy
-import           Polysemy.State
+import           Control.Monad.ST (ST, runST)
+import           Data.Comp.Sum
+import           Data.Functor.Compose (Compose(..))
+import           Data.Functor.Foldable (Fix(..), cata, unfix)
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import           Data.STRef (STRef, newSTRef, readSTRef, modifySTRef)
+import           Data.Text (Text)
+import           Data.Vector (Vector, singleton, (!))
+import           Numeric.Natural (Natural)
 
-import           Core
-import           Core.Eval
-import           Core.EvalEnv
-import           Expr
-import           Surface
-import           Typed
-import           Display
+data Expr a where
+  Lam :: a -> Expr a
+  App :: a -> a -> Expr a
+  Bound :: Natural -> Expr a
 
-someFunc :: IO ()
-someFunc = do
-  let
-    (e :: Fix SurfaceE) = mkArrow
-      se
-      ("a", Inline R0, Inline S)
-      (mkCon se "Type")
-      (mkArrow se
-               ("x", Inline R0, Inline S)
-               (mkFree se "a")
-               (mkFree se "a")
-      )
-  let
-    ctx =
-      Map.fromList [("thing", (mkFree ce "thing")), ("dude", mkFree ce "dude")]
-  putStrLn . cata display $ e
-  putStrLn . cata display . toCore $ e
-  putStrLn
-    . cata display
-    . run
-    . evalState @(Map.Map String (Fix CoreE)) ctx
-    . runSymLookupState @String @(Fix CoreE)
-    . eval
-    . toCore
-    $ e
+deriving instance Functor Expr
+
+data Data a where
+  List :: Vector a -> Data a
+  Case :: a -> Map Natural a -> Data a
+  In :: Natural -> a -> Data a
+  Out :: Natural -> a -> Data a
+
+deriving instance Functor Data 
+
+data Type a where
+  Ann :: a -> a -> Type a 
+  Arr :: a -> a -> Type a
+  Type :: Natural -> Type a
+
+deriving instance Functor Type 
+
+type TypedTerm = Expr :+: Data :+: Type
+
+type Term = Expr :+: Data
+
+type EvalTerm s = Stack s -> ST s (Fix Term)
+
+data Stack s = Stack [STRef s (Thunk s)]
+
+data Thunk s = Thunk (Either (EvalTerm s) (Fix Term)) (Stack s)
+
+push a (Stack xs) = Stack (a:xs) 
+
+index :: Natural -> Stack s -> Maybe (STRef s (Thunk s))
+index _ (Stack []) = Nothing
+index 0 (Stack (x:_)) = Just x
+index n (Stack (_:rest)) = index (n - 1) (Stack rest)
+
+forceThunk :: Thunk s -> ST s (Fix Term)
+forceThunk (Thunk (Left f) st) = f st
+forceThunk (Thunk (Right value) _) = pure value
+
+eval :: Fix Term -> Fix Term 
+eval term = runST $ (cata f term) (Stack []) where
+
+  f :: Term (EvalTerm s) -> (EvalTerm s) 
+  f = caseF goE goD  where
+
+    goE :: Expr (EvalTerm s) -> EvalTerm s 
+    goE (Lam body) st = body st 
+    goE (App func input) st = do
+      ref <- newSTRef (Thunk (Left input) st)
+      func (push ref st)
+    goE (Bound i) st = do
+      case index i st of
+        Just ref -> forceThunk =<< readSTRef ref
+        Nothing -> error "Bound Symbol Error"
+
+    goD :: Data (EvalTerm s) -> EvalTerm s
+    goD (List xs) st = do
+      xs' <- sequence $ xs <*> pure st
+      pure . Fix . inj $ List xs'
+    goD (Case x paths) st = do
+      subject <- x st
+      caseF
+        (error "Case exception")
+        (\case 
+            In i value ->
+              case Map.lookup i paths of
+                Just path -> do
+                  ref <- newSTRef (Thunk (Right value) st)
+                  path (push ref st)
+                Nothing -> error "Unhandled case exception"
+            _ -> error "Case value error"
+        )
+        (unfix subject)
+    goD (In i x) st = Fix . inj . In i <$> x st
+    goD (Out i x) st = do
+      subject <- x st
+      caseF
+        (error "Must index a list")
+        (\case
+            List xs -> pure (xs ! fromIntegral i)
+            _ -> error "Must index a list")
+        (unfix subject)
